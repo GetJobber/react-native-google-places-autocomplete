706ca66e3e2c1e62aec7e11f88ac0c8463be2196
Ryan Berry <ryan.b@getjobber.com>
2021-05-12 09:11:43 -0600

    Pass current query into debounced request function

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -479,7 +479,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
     }
   };
 
-  const _request = (text, requestUrl) => {
+  const _request = (text, requestUrl, query) => {
     _abortRequests();
     if (supportedPlatform() && text && text.length >= props.minLength) {
       const request = new XMLHttpRequest();
@@ -531,7 +531,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
         `${url}/place/autocomplete/json?input=` +
           encodeURIComponent(text) +
           '&' +
-          Qs.stringify(props.query),
+          Qs.stringify(query),
       );
 
       request.withCredentials = requestShouldUseWithCredentials();
@@ -549,7 +549,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
 
   const _onChangeText = (text) => {
     setStateText(text);
-    debounceData(text, props.requestUrl);
+    debounceData(text, props.requestUrl, props.query);
   };
 
   const _handleChangeText = (text) => {
